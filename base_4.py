2ffe9ae85edd95f62798f9dafc6f7043c68d5a7f
Alaaddin Hiefield <alaaddin.hiefield@gmail.com>
2019-01-16 17:08:43 -0800

    Added functions to improve code reusability
    just added some functions

--- base_4.py
+++ base_4.py
@@ -1,7 +1,21 @@
-x  = int(input("Choose a number to find it's base counterpart.\n"))
+def get_number():
+    """
+    This function takes no arguments, and is 
+    designed to fetch the number that the user
+    would like to convert into a different base.
+    """
+    return int(input("Choose a number to find it's base counterpart.\n"))
 
-base = int(input("Which base is the number system?\n"))
+def get_base():
+    """
+    This function takes no arguments, and is 
+    designed to fetch the base that the user
+    would like to convert into their number into.
+    """
+    return int(input("Which base is the number system?\n"))
 
+number = get_number()
+base = get_base()
 
 y = int(x / base)
 
